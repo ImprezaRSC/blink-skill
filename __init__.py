e737ee5072b1648d1d9bf1f627c7c8355e358207
ImprezaRSC <46632364+ImprezaRSC@users.noreply.github.com>
2019-05-05 10:58:22 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 from mycroft import MycroftSkill, intent_file_handler
 import time
 import RPi.GPIO as GPIO
-
+import subprocess as sub
 
 class Blink(MycroftSkill):
     def __init__(self):
@@ -11,16 +11,18 @@ class Blink(MycroftSkill):
     def handle_blink(self, message):
         self.speak_dialog('blink')
 
-        GPIO.setmode(GPIO.BOARD)
-        green=11
-        GPIO.setup(green,GPIO.OUT)
-        GPIO.output(green,True)
-        time.sleep(1)
-        GPIO.output(green,False)
-        time.sleep(1)
-        GPIO.output(green,True)
-        time.sleep(1)
-        GPIO.output(green,False)
+        #GPIO.setmode(GPIO.BOARD)
+        #green=11
+        #GPIO.setup(green,GPIO.OUT)
+        #GPIO.output(green,True)
+        #time.sleep(1)
+        #GPIO.output(green,False)
+        #time.sleep(1)
+        #GPIO.output(green,True)
+        #time.sleep(1)
+        #GPIO.output(green,False)
+        s = "./blink.c"
+        subprocess.call([s],shell=True)
         GPIO.cleanup()
 
 def create_skill():
